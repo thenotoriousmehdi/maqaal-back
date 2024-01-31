dcb380785c6f129105c422632fbf4578f8b99ad3
rizd-7 <128991975+rizd-7@users.noreply.github.com>
2024-01-31 08:41:58 +0100

    fill your bd creds

--- backend/database.py
+++ backend/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base,sessionmaker
 
 #database name is sensitive to blank space
-engine=create_engine('postgresql://postgres:ushuaia@localhost/PizzaDelivery',
+engine=create_engine('postgresql://postgres:<...>@localhost/<...>',
     echo=True
 )
 
